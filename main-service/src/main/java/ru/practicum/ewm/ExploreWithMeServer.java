f7f14afcf8c2963d95b1651eef535168e284c9dd
NovoseltsevVladimir <novoseltsev-v-vladimir@yandex.ru>
2025-11-27 20:18:13 +0300

    Исправить pom файлы и Docker

--- main-service/src/main/java/ru/practicum/ewm/ExploreWithMeServer.java
+++ main-service/src/main/java/ru/practicum/ewm/ExploreWithMeServer.java
@@ -2,12 +2,11 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.practicum.ewm.stats.service.StatServiceApp;
 
 @SpringBootApplication
 public class ExploreWithMeServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(StatServiceApp.class, args);
+        SpringApplication.run(ExploreWithMeServer.class, args);
     }
 }
