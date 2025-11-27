0af9983d22a409621877caec0ea0149774e0504b
NovoseltsevVladimir <novoseltsev-v-vladimir@yandex.ru>
2025-11-27 23:04:53 +0300

    Исправить запрос

--- stats/stats-service/src/main/java/ru/practicum/ewm/stats/service/repository/StatServiceRepository.java
+++ stats/stats-service/src/main/java/ru/practicum/ewm/stats/service/repository/StatServiceRepository.java
@@ -12,7 +12,8 @@ import java.util.List;
 public interface StatServiceRepository extends JpaRepository<Hit, Integer> {
 
     @Query("""
-            SELECT new ru.practicum.ewm.stats.dto.StatsDto(hit.app, hit.uri, COUNT(DISTINCT hit.ip))
+            SELECT new ru.practicum.ewm.stats.dto.StatsDto(hit.app, hit.uri, 
+            COUNT(DISTINCT hit.ip))
             FROM Hit hit
             WHERE hit.timestamp BETWEEN :start AND :end
             GROUP BY hit.app, hit.uri
@@ -33,7 +34,8 @@ public interface StatServiceRepository extends JpaRepository<Hit, Integer> {
 
 
     @Query("""
-            SELECT new ru.practicum.ewm.stats.dto.StatsDto(hit.app, hit.uri, COUNT(hit.ip))
+            SELECT new ru.practicum.ewm.stats.dto.StatsDto(hit.app, hit.uri,
+            COUNT(DISTINCT hit.ip))
             FROM Hit hit
             WHERE hit.timestamp BETWEEN :start AND :end
             AND hit.uri IN :uris
